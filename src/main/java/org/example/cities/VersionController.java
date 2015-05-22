3d32e1e75785250ffbdd872c797d418df47fe222
Sharath Sahadevan <ssahadevan@pivotal.io>
2015-05-22 09:16:41 -0700

    Test Multi site

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings - SS Version Number= 19!";
+        return "Greetings - SS Version Number= 20!";
     }
 }
